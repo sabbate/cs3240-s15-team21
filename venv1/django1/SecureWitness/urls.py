f5e7ff4ced7ce96e47c42e358f0d4b4d9b7a1b22
Yixuan Nie <yn9sa@virginia.edu>
2015-04-05 18:01:32 -0400

    login

--- venv1/django1/SecureWitness/urls.py
+++ venv1/django1/SecureWitness/urls.py
@@ -1,13 +1,13 @@
 __author__ = 'Yixuan1'
-from django.conf.urls import  url
+from django.conf.urls import patterns, url
 
 from . import views
 
-urlpatterns = [ url(r'^login/$', views.login),
-                        url(r'^auth/$', views.auth_view),
-                        url(r'^logout/$', views.logout),
-                        url(r'^loggedin/$', views.loggedin),
-                        url(r'^invalid/$', views.invalid),
+urlpatterns = [ url(r'^/login/$', views.login),
+                        url(r'^/auth/$', views.auth_view),
+                        url(r'^/logout/$', views.logout),
+                        url(r'^/loggedin/$', views.loggedin),
+                        url(r'^/invalid/$', views.invalid),
                     ]
 '''
                        url(r'^login/$', 'django.contrib.auth.views.login',name="my_login"),
--- venv1/django1/SecureWitness/views.py
+++ venv1/django1/SecureWitness/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
 
 # Create your views here.
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('../loggedin')
+    else:
+        return HttpResponseRedirect('../invalid')
+
+def loggedin(request):
+    return render_to_response('loggedin.html',
+        {'full_name': request.user.username})
+
+def invalid(request):
+    return render_to_response('invalid.html')
+
+def logout(request):
+    auth.logout(request)
+    return render_to_response('logout.html')
