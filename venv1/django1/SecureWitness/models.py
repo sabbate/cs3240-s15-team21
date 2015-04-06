1bdfb640b764b619a72db2236262e0ec597724a4
smm8ec <sarahmohamed2007@yahoo.com>
2015-04-06 17:34:55 -0400

    Attempted merge with pre-override

--- venv1/django1/SecureWitness/models.py
+++ venv1/django1/SecureWitness/models.py
@@ -7,7 +7,7 @@ class Users(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     reg_date = models.DateTimeField('date of registration')
-    privilege = models.CharField(max_length=100)
+    privilege = models.CharField(max_length=100, default='DEFAULT VALUE')
 
 class Reports(models.Model):
 	RID = models.AutoField(primary_key=True)
