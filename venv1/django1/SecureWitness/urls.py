00cd5930e7b13e552c21ded4c602563d06e8cc37
Grant_Oxer <gwo8gy@virginia.edu>
2015-04-06 14:28:11 -0400

    Potential merge fix.

--- venv1/django1/SecureWitness/urls.py
+++ venv1/django1/SecureWitness/urls.py
@@ -7,4 +7,15 @@ urlpatterns = patterns('',
                        # url(r'^/groups', views.GroupDetailView.as_view(), name='group'),
                        url(r'^(?P<pk>\d+)/$', views.GroupDetailView.as_view(), name='group'),
                        url(r'^(?P<pk>\d+)/$', views.ReportIndexView.as_view(), name='report_index'),
+                       url(r'^/login/$', views.login),
+                        url(r'^/auth/$', views.auth_view),
+                        url(r'^/logout/$', views.logout),
+                        url(r'^/loggedin/$', views.loggedin),
+                        url(r'^/invalid/$', views.invalid),
                        )
+
+'''
+                       url(r'^login/$', 'django.contrib.auth.views.login',name="my_login"),
+                       url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+
+'''
--- venv1/django1/SecureWitness/views.py
+++ venv1/django1/SecureWitness/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response, render
 from django.views import generic
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
 
 from SecureWitness.models import *
 
@@ -26,3 +30,35 @@ class ReportIndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five uploaded reports."""
         return Report.objects.order_by('-RID')[:5]
+
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('../loggedin')
+    else:
+        return HttpResponseRedirect('../invalid')
+
+
+def loggedin(request):
+    return render_to_response('loggedin.html',
+        {'full_name': request.user.username})
+
+
+def invalid(request):
+    return render_to_response('invalid.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render_to_response('logout.html')
