295816885845e370fa6f678ffff3a91a74ce7949
Yixuan Nie <yn9sa@virginia.edu>
2015-04-13 15:49:32 -0400

    updated login & logout templates and views

--- venv1/django1/SecureWitness/urls.py
+++ venv1/django1/SecureWitness/urls.py
@@ -3,11 +3,26 @@ from django.conf.urls import patterns, url
 
 from . import views
 
-urlpatterns = [ url(r'^/login/$', views.login),
-                        url(r'^/auth/$', views.auth_view),
-                        url(r'^/logout/$', views.logout),
-                        url(r'^/loggedin/$', views.loggedin),
-                        url(r'^/invalid/$', views.invalid),
+urlpatterns = [ url(r'^/account/login/$', views.login),
+                        url(r'^/account/auth/$', views.auth_view),
+                        url(r'^/account/logout/$', views.logout),
+                        url(r'^/account/loggedin/$', views.loggedin),
+                        url(r'^/account/invalid/$', views.invalid),
+                        url(r'^/account/register/$', views.register_user),
+                        url(r'^/account/register_success/$', views.register_success),
+                        url(r'^/admin/$', views.admin),
+                        url(r'^/admin_assigned/$', views.admin_assigned),
+                        url(r'^/admin_already_assigned/$', views.admin_already_assigned),
+                        url(r'^/assigning_admin_view/$', views.assigning_admin_view),
+                        url(r'^/admin_assign_failed/$', views.admin_assign_failed),
+                        url(r'^/user_suspended/$', views.user_suspended),
+                        url(r'^/user_already_suspended/$', views.user_already_suspended),
+                        url(r'^/suspend_user_view/$', views.suspend_user_view),
+                        url(r'^/user_suspend_failed/$', views.user_suspend_failed)
+
+
+
+
                     ]
 '''
                        url(r'^login/$', 'django.contrib.auth.views.login',name="my_login"),
--- venv1/django1/SecureWitness/views.py
+++ venv1/django1/SecureWitness/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -17,17 +20,96 @@ def auth_view(request):
 
     if user is not None:
         auth.login(request, user)
-        return HttpResponseRedirect('../loggedin')
+        if(user.is_superuser):
+            return HttpResponseRedirect('../../admin')
+        else:
+            return HttpResponseRedirect('../loggedin')
     else:
         return HttpResponseRedirect('../invalid')
 
+@login_required
 def loggedin(request):
     return render_to_response('loggedin.html',
         {'full_name': request.user.username})
 
+@login_required
+def admin(request):
+    c = {}
+    c.update(csrf(request))
+    #c['username'] =  request.newAdmin.username
+    return render_to_response('admin.html',
+        c)
+
+def suspend_user_view(request):
+    username = request.POST.get('username_suspend', '')
+    try:
+        suspend = User.objects.get(username=username)
+    except:
+        return HttpResponseRedirect('../user_suspend_failed')
+
+    if suspend.is_active:
+        suspend.is_active = False
+        suspend.save()
+        if not suspend.is_active :
+            return HttpResponseRedirect('../user_suspended')
+    else:
+        return HttpResponseRedirect('../user_already_suspended')
+
+def assigning_admin_view(request):
+    username = request.POST.get('username_admin', '')
+
+    try:
+        newAdmin = User.objects.get(username=username)
+    except:
+        return HttpResponseRedirect('../admin_assign_failed')
+
+    if not newAdmin.is_superuser:
+        newAdmin.is_superuser = True
+        newAdmin.save()
+        if newAdmin.is_superuser:
+            return HttpResponseRedirect('../admin_assigned')
+    else:
+        return HttpResponseRedirect('../admin_already_assigned')
+
+
+
 def invalid(request):
     return render_to_response('invalid.html')
 
+@login_required
 def logout(request):
     auth.logout(request)
     return render_to_response('logout.html')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('../register_success')
+    args = {}
+    args.update(csrf(request))
+
+    args['form'] = UserCreationForm()
+    return render_to_response('register.html', args)
+
+def register_success(request):
+    return render_to_response('register_success.html')
+
+def admin_assigned(request):
+    return render_to_response('admin_assigned.html')
+
+def admin_already_assigned(request):
+    return render_to_response('admin_already_assigned.html')
+
+def admin_assign_failed(request):
+    return render_to_response('admin_assign_failed.html')
+
+def user_suspended(request):
+    return render_to_response('user_suspended.html')
+
+def user_already_suspended(request):
+    return render_to_response('user_already_suspended.html')
+
+def user_suspend_failed(request):
+    return render_to_response('user_suspend_failed.html')
