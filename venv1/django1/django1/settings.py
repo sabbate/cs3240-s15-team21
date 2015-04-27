5321ef217967581df9f0193b2371959187c3922f
Sam Abbate <sja4cm@virginia.edu>
2015-04-26 19:42:51 -0500

    settings and wsgi updated

--- venv1/django1/django1/settings.py
+++ venv1/django1/django1/settings.py
@@ -8,10 +8,29 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+#commented out to make room for heroku stuff above
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#import os
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
