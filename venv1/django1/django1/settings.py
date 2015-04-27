c94622d1352e42a49b86e60e86de3f8ad65cfa88
Sam Abbate <sja4cm@virginia.edu>
2015-04-26 21:20:42 -0500

    modified settings.py

--- venv1/django1/django1/settings.py
+++ venv1/django1/django1/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
+# Enable Connection Pooling
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
